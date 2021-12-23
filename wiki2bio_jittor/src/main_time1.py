db7e74c30805e74b093515ce9e0133968638fde0
namezhenzhang <704543161@qq.com>
2021-12-24 02:52:28 +0800

    添加了时间测试程序

---
+++ wiki2bio_jittor/src/main_time1.py
@@ -0,0 +1,315 @@
+
+import logger
+from configs import get_args_parser,  get_args, save_args
+import random
+import numpy  as np
+import jittor
+import jittor as jt
+import time, os, sys, shutil
+# sys.path.append('./')
+from tqdm import tqdm
+from SeqUnit import *
+from preprocess import *
+from PythonROUGE import PythonROUGE
+from DataLoader import DataLoader
+from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction
+from tensorboardX import SummaryWriter
+
+log = logger.get_logger(__name__)
+
+def set_seed(seed):
+    # random.seed(seed)
+    # np.random.seed(seed)
+    jittor.misc.set_global_seed(seed)
+    # torch.manual_seed(seed)
+    # if args.n_gpu > 0:
+    #     torch.cuda.manual_seed_all(seed)
+
+def do_before_running():
+    args = get_args_parser()
+    logger.setup_applevel_logger(file_name = args.logger_file_name)
+    log.info(f"output dir: '{args.output_dir}'")
+    save_args()
+    
+    log.info(f"set seed {args.seed}")
+    set_seed(args.seed)
+    return args
+
+
+global_step = 0
+
+def train(model, train_dataloader,dev_dataloader,test_dataloader,writer):
+    global global_step
+    optimizer = nn.Adam(model.parameters(), args.learning_rate, eps=1e-8, betas=(0.9, 0.999))
+    total_loss, start_time = 0.0, time.time()
+    loss = 0.0
+    # for num_eopch in range(args.epoch):
+
+    wormup = 10
+    global_steps = 0
+    test_steps = 200
+    start_time = 0
+    end_time = 0
+
+
+    # with tqdm(total=len(train_dataloader)) as Tqdm:
+    
+    it = iter(train_dataloader)
+    model.train()
+    jt.sync_all(True)
+    for _ in tqdm(range(wormup)):
+        x = next(it)
+        loss = model(**x)
+        loss.sync()
+        optimizer.step(loss)
+    jt.sync_all(True)
+
+    it = iter(train_dataloader)
+    start = time.time()
+    for _ in tqdm(range(test_steps)):
+        x = next(it)
+        loss = model(**x)
+        loss.sync()
+        optimizer.step(loss)
+    jt.sync_all(True)
+    end = time.time()
+    print((end-start)/test_steps)
+
+
+        
+        
+            
+            
+            # if (global_step % args.report == 0):
+            #     cost_time = time.time() - start_time
+            #     log.info("%d : loss = %.3f, time = %.3f " % (global_step // args.report, total_loss/args.report, cost_time))
+            #     total_loss, start_time = 0.0, time.time()
+            #     #TODO保存模型
+            #     if global_step // args.report >= 1: 
+            #         ksave_dir = save_model(model, save_dir, global_step // args.report)
+            #         evaluate(model, dev_dataloader, ksave_dir, 'valid')
+
+def test(model, dataloader,writer):
+    evaluate(model, dataloader, save_dir, writer,'test')
+
+def save_model(model, save_dir, cnt):
+    new_dir = save_dir + 'checkpoints' + '/' 
+    if not os.path.exists(new_dir):
+        os.mkdir(new_dir)
+    nnew_dir = new_dir + str(cnt) + '/'
+    if not os.path.exists(nnew_dir):
+        os.mkdir(nnew_dir)
+    model.save(nnew_dir+'model.ckpt')
+    return nnew_dir
+
+def write_word(pred_list, save_dir, name):
+    ss = open(save_dir + name, "w+")
+    for item in pred_list:
+        ss.write(" ".join(item) + '\n')
+@jittor.no_grad()
+def evaluate(model, dataloader, ksave_dir, writer,mode='valid'):
+    if mode == 'valid':
+        # texts_path = "original_data/valid.summary"
+        texts_path = os.path.join(args.root_dir,"processed_data/valid/valid.box.val")
+        gold_path = os.path.join(args.root_dir,'processed_data/valid/valid_split_for_rouge/gold_summary_')
+        # evalset = dataloader.dev_set
+    else:
+        # texts_path = "original_data/test.summary"
+        texts_path = os.path.join(args.root_dir,"processed_data/test/test.box.val")
+        gold_path = os.path.join(args.root_dir,'processed_data/test/test_split_for_rouge/gold_summary_')
+        # evalset = dataloader.test_set
+    
+    # for copy words from the infoboxes
+    texts = open(texts_path, 'r').read().strip().split('\n')
+    texts = [list(t.strip().split()) for t in texts]
+    v = Vocab()
+
+    # with copy
+    pred_list, pred_list_copy, gold_list = [], [], []
+    pred_unk, pred_mask = [], []
+    
+    k = 0
+    with tqdm(total=len(dataloader)) as Tqdm:
+        for x in dataloader:
+            model.eval()
+            Tqdm.update(1)
+            predictions, atts = model.generate(**x)
+
+            atts = np.squeeze(np.array(atts),axis=-1)
+            idx = 0
+            for summary in np.array(predictions):
+                with open(pred_path + str(k), 'w') as sw:
+                    summary = list(summary)
+                    if 2 in summary:
+                        summary = summary[:summary.index(2)] if summary[0] != 2 else [2]
+                    real_sum, unk_sum, mask_sum = [], [], []
+                    for tk, tid in enumerate(summary):
+                        if tid == 3:
+                            sub = texts[k][np.argmax(atts[tk,: len(texts[k]),idx])]
+                            real_sum.append(sub)
+                            mask_sum.append("**" + str(sub) + "**")
+                        else:
+                            real_sum.append(v.id2word(tid))
+                            mask_sum.append(v.id2word(tid))
+                        unk_sum.append(v.id2word(tid))
+                    sw.write(" ".join([str(x) for x in real_sum]) + '\n')
+                    pred_list.append([str(x) for x in real_sum])
+                    pred_unk.append([str(x) for x in unk_sum])
+                    pred_mask.append([str(x) for x in mask_sum])
+                    k += 1
+                    idx += 1
+
+    write_word(pred_mask, ksave_dir, mode + "_summary_copy.txt")
+    write_word(pred_unk, ksave_dir, mode + "_summary_unk.txt")
+
+
+    for tk in range(k):
+        with open(gold_path + str(tk), 'r') as g:
+            gold_list.append([g.read().strip().split()])
+
+    gold_set = [[gold_path + str(i)] for i in range(k)]
+    pred_set = [pred_path + str(i) for i in range(k)]
+
+    recall, precision, F_measure = PythonROUGE(pred_set, gold_set, ngram_order=4)
+    bleu = corpus_bleu(gold_list, pred_list)
+    copy_result = "with copy F_measure: %s Recall: %s Precision: %s BLEU: %s ." % \
+    (str(F_measure), str(recall), str(precision), str(bleu))
+    log.info(copy_result)
+    try:
+        writer.add_scalar('F_measure copy', F_measure, global_step=global_step)
+        writer.add_scalar('Recall copy', recall, global_step=global_step)
+        writer.add_scalar('Precision copy', precision, global_step=global_step)
+    except:
+        pass
+    try:
+        writer.add_scalar('BLEU copy', bleu, global_step=global_step)
+    except:
+        pass
+
+
+    # print copy_result
+
+    for tk in range(k):
+        with open(pred_path + str(tk), 'w') as sw:
+            sw.write(" ".join(pred_unk[tk]) + '\n')
+
+    recall, precision, F_measure = PythonROUGE(pred_set, gold_set, ngram_order=4)
+    bleu = corpus_bleu(gold_list, pred_unk)
+    nocopy_result = "without copy F_measure: %s Recall: %s Precision: %s BLEU: %s ." % \
+    (str(F_measure), str(recall), str(precision), str(bleu))
+    log.info(nocopy_result)
+    try:
+        writer.add_scalar('F_measure nocopy', F_measure, global_step=global_step)
+        writer.add_scalar('Recall nocopy', recall, global_step=global_step)
+        writer.add_scalar('Precision nocopy', precision, global_step=global_step)
+    except:
+        pass
+    try:
+        writer.add_scalar('BLEU nocopy', bleu, global_step=global_step)
+    except:
+        pass
+
+    # print nocopy_result
+    result = copy_result + nocopy_result 
+    # print result
+    # if mode == 'valid':
+    #     log.info(result)
+
+    return result
+
+def copy_file(dst, src=os.path.dirname(os.path.abspath(__file__))):
+    files = os.listdir(src)
+    saved_files = []
+    for file in files:
+        file_ext = file.split('.')[-1]
+        if file_ext=='py':
+            saved_files.append(file)
+            shutil.copy(os.path.join(src,file), dst)
+    log.info(f'saved files {saved_files} to {dst}')
+
+# def test_main():
+#     print(os.path.join(args.root_dir,args.dir))
+#     train_dataloader = DataLoader(os.path.join(args.root_dir,args.dir), args.limits, 'dev').set_attrs(batch_size=32, shuffle=True)
+#     with tqdm(total=len(train_dataloader)) as Tqdm:
+#         for i in train_dataloader:
+#             Tqdm.update(1)
+#             pass
+#     print(train_dataloader.num_error)
+
+
+
+def main():
+    copy_file(save_file_dir)
+    if args.mode == 'train':
+        train_dataloader = DataLoader(os.path.join(args.root_dir,args.dir), args.limits, 'train').set_attrs(batch_size=args.batch_size, shuffle=True)
+        dev_dataloader = DataLoader(os.path.join(args.root_dir,args.dir), args.limits, 'dev').set_attrs(batch_size=args.batch_size_valid, shuffle=False)
+    test_dataloader = DataLoader(os.path.join(args.root_dir,args.dir), args.limits, 'test').set_attrs(batch_size=args.batch_size_valid, shuffle=False)
+    if args.mode == 'train':
+        log.info(f'erro_data: train {train_dataloader.num_error}, dev {dev_dataloader.num_error}, test {test_dataloader.num_error}')
+    else:
+        log.info(f'erro_data: test {test_dataloader.num_error}')
+
+    model = SeqUnit(batch_size=args.batch_size, hidden_size=args.hidden_size, emb_size=args.emb_size,
+                        field_size=args.field_size, pos_size=args.pos_size, field_vocab=args.field_vocab,
+                        source_vocab=args.source_vocab, position_vocab=args.position_vocab,
+                        target_vocab=args.target_vocab, scope_name="seq2seq", name="seq2seq",
+                        field_concat=args.field, position_concat=args.position,
+                        fgate_enc=args.fgate_encoder, dual_att=args.dual_attention, decoder_add_pos=args.decoder_pos,
+                        encoder_add_pos=args.encoder_pos, learning_rate=args.learning_rate)
+    writer = SummaryWriter(args.output_dir + '/tensorboard/')
+    if args.load != '0':
+        model.load(save_dir)
+    if args.resume != None:
+        log.info(f'resume, path: {args.resume}')
+        model.load(args.resume)
+        
+    if args.mode == 'train':
+        train(model, train_dataloader, dev_dataloader, test_dataloader, writer)
+        # test(model, test_dataloader, writer)
+    else:
+        test(model,test_dataloader,writer)
+
+if __name__=='__main__':
+    args = do_before_running()
+
+    os.chdir(args.root_dir)
+    last_best = 0.0
+    gold_path_test = 'processed_data/test/test_split_for_rouge/gold_summary_'
+    gold_path_valid = 'processed_data/valid/valid_split_for_rouge/gold_summary_'
+    # test phase
+    if args.load != "0":
+        save_dir = os.path.dirname(args.output_dir)+ f'/{args.load}' + '/res/'
+        save_file_dir = save_dir + '/files/'
+        pred_dir = os.path.dirname(args.output_dir)+ f'/{args.load}' + '/evaluation/'
+        if not os.path.exists(pred_dir):
+            os.mkdir(pred_dir)
+        if not os.path.exists(save_file_dir):
+            os.mkdir(save_file_dir)
+        pred_path = pred_dir + 'pred_summary_'
+        pred_beam_path = pred_dir + 'beam_summary_'
+    # train phase
+    else:
+        save_dir = args.output_dir + '/res/'
+        save_file_dir = save_dir + 'files/'
+        pred_dir = args.output_dir + '/evaluation/'
+        os.mkdir(save_dir)
+        if not os.path.exists(pred_dir):
+            os.mkdir(pred_dir)
+        if not os.path.exists(save_file_dir):
+            os.mkdir(save_file_dir)
+        pred_path = pred_dir + 'pred_summary_'
+        pred_beam_path = pred_dir + 'beam_summary_'
+
+    log_file = args.logger_file_name
+
+    if jittor.compiler.has_cuda and args.use_cuda:
+        jittor.flags.use_cuda = 1
+        log.info('use cuda!')
+    else:
+        log.info('use cpu!')
+
+    main()
+
+    log.info('all finished')
+
+
--- wiki2bio_jittor/src/run.sh
+++ wiki2bio_jittor/src/run.sh
@@ -5,14 +5,15 @@ CUDA_VISIBLE_DEVICES=2,3,4
 
 nohup python wiki2bio_jittor/src/main.py \
 --mode train \
---batch_size 32 \
---seed 666 \
---batch_size_valid 512 \
+--batch_size 64 \
+--seed 100 \
+--batch_size_valid 256 \
 --accumulation 1 \
---learning_rate 0.0005 \
+--learning_rate 0.0003 \
 --use_cuda True \
---epoch 20 \
---dual_attention False \
->no_dual.out 2>&1 &
+--epoch 10 \
+--dual_attention True \
+--resume /home/zhangzhen/dir1/wiki2bio/wiki2bio_jittor/outputlogs/00026/res/checkpoints/9/model.ckpt \
+>dual_3.out 2>&1 &
 
 # https://cg.cs.tsinghua.edu.cn/jittor/assets/cuda11.2_cudnn8_linux.tgz
