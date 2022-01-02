6d65da9b11a4f2d418f084a93cb4aa620255b43a
namezhenzhang <704543161@qq.com>
2022-01-02 22:53:09 +0800

    修改了run.sh

--- wiki2bio_jittor/src/run.sh
+++ wiki2bio_jittor/src/run.sh
@@ -1,7 +1,7 @@
 # export JT_SYNC=1
 # export trace_py_var=3
 
-CUDA_VISIBLE_DEVICES=2,3,4
+# CUDA_VISIBLE_DEVICES=2,3,4
 
 # nohup python wiki2bio_jittor/src/main.py \
 # --mode test \
@@ -19,13 +19,14 @@ CUDA_VISIBLE_DEVICES=2,3,4
 # https://cg.cs.tsinghua.edu.cn/jittor/assets/cuda11.2_cudnn8_linux.tgz
 
 python wiki2bio_jittor/src/main.py \
---mode test \
+--mode train \
 --batch_size 64 \
 --seed 100 \
---batch_size_valid 128 \
+--batch_size_valid 256 \
 --accumulation 1 \
 --learning_rate 0.0003 \
 --use_cuda True \
---epoch 10 \
---dual_attention True \
---resume /home/zhangzhen/dir1/wiki2bio/wiki2bio_jittor/outputlogs/00029/res/checkpoints/17/model.ckpt
+--epoch 30 \
+# --dual_attention True \
+
+# --resume /home/zhangzhen/dir1/wiki2bio/wiki2bio_jittor/outputlogs/00029/res/checkpoints/17/model.ckpt
